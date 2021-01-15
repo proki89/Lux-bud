ee77f9d8303ba41ab46b315c334f777460b3b272
Agata Cimaj <89lukasz.prokop@gmail.com>
2021-01-15 21:59:48 +0100

    zmiana banana

--- js/main.js
+++ js/main.js
@@ -1,27 +1,131 @@
-$('.team-carousel').slick({
-    autoplay: true,
-    autoplaySpeed: 3500,
-    mobileFirst: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 1600,
-            settings: {
-                slidesToShow: 4
-            }
-        }
-    ]
-});
+// $('.team-carousel').slick({
+//     autoplay: true,
+//     autoplaySpeed: 3500,
+//     mobileFirst: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 2
+//             }
+//         },
+//         {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         },
+//         {
+//             breakpoint: 1600,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }
+//     ]
+// });
 
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+ 
+  
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoPlay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+
+  });
+  $('.single-item').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true,
+  });
+
+  $('.one-time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+		
